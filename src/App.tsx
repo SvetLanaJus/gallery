a167741a1048edb20fbe5c0b0a5e0be7bee4d275
SvetLanaJus <svetlana.juscenko@gokstadakademiet.no>
2026-08-22 15:12:50 +0200

    Add main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { Routes, Route } from "react-router-dom"
+import MainLayout from "./layout/MainLayout"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
-import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
-    <div className="bg-black min-h-screen text-white">
-      <Navbar />
-
-      <Routes>
+    <Routes>
+      <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   )
 }
